00f4bf7fce1432d2033bf979b1068566d676e98b
mauriciobrian793-lgtm <mauriciobrian793@gmail.com>
2026-05-27 19:47:45 -0700

    first commit

---
+++ database.py
@@ -0,0 +1,38 @@
+from pymongo import MongoClient
+
+MONGO_URL = "mongodb+srv://mauriciobrian793_db_user:SJQ5sCtQVDyASCzp@fishyeconomybotcluster.ijsludq.mongodb.net/"
+
+client = MongoClient(MONGO_URL)
+
+db = client["fishy_bot"]
+economy = db["economy"]
+
+def get_user(guild_id, user_id):
+    data = economy.find_one({
+        "guild_id": str(guild_id),
+        "user_id": str(user_id)
+    })
+
+    if not data:
+        data = {
+            "guild_id": str(guild_id),
+            "user_id": str(user_id),
+            "balance": 0
+        }
+        economy.insert_one(data)
+
+    return data
+
+
+def add_money(guild_id, user_id, amount):
+    economy.update_one(
+        {"guild_id": str(guild_id), "user_id": str(user_id)},
+        {"$inc": {"balance": amount}}
+    )
+
+
+def remove_money(guild_id, user_id, amount):
+    economy.update_one(
+        {"guild_id": str(guild_id), "user_id": str(user_id)},
+        {"$inc": {"balance": -amount}}
+    )
