963d7043584b7f9a3d2191beed7b94eb2b675606
mauriciobrian793-lgtm <mauriciobrian793@gmail.com>
2026-05-30 00:20:17 -0700

    .

--- cogs/collect_income.py
+++ cogs/collect_income.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 from utils import check_cooldown
 from pymongo import ReturnDocument
+from utils import get_settings
 
 
 class CollectIncome(commands.Cog):
