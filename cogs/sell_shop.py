8f73f0778b8caf4acfd52cded3708d83ed6fda06
mauriciobrian793-lgtm <mauriciobrian793@gmail.com>
2026-05-30 18:03:32 -0700

    m

--- cogs/sell_shop.py
+++ cogs/sell_shop.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-
+from discord import app_commands
 
 class SellView(discord.ui.View):
 
--- cogs/shop_inventory.py
+++ cogs/shop_inventory.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 
 
 class Inventory(commands.Cog):
